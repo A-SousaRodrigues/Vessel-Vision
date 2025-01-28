24428ee19977468040b5075067eaf1c6c05bcfc8
Alex Sousa Rodrigues <alexsousarodrigues9954@gmail.com>
2025-01-28 05:11:27 +0000

    ADD django-summernote for Question Model in admin

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import GameProgress, Question, Score
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Question)
+class QuestionAdmin(SummernoteModelAdmin):
+
+    list_display = ('question_text', 'option_1', 'correct_answer')
+    search_fields = ['question_text']
+    summernote_fields = ('question_text',)
 
 # Register your models here.
 admin.site.register(GameProgress)
-admin.site.register(Question)
 admin.site.register(Score)
