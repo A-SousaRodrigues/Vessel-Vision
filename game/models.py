992033ad25d91ed44d5969b0afb0a46922099bd1
Alex Sousa Rodrigues <alexsousarodrigues9954@gmail.com>
2025-01-28 04:25:28 +0000

    Create GameProgress Model and Migrate

--- game/models.py
+++ game/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# GameProgress Model
+class GameProgress(models.Model):
+    """
+    Stores the game progress related to :model:`auth.User`.
+    """
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_question = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
+    progress_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    completed = models.BooleanField(default=False)
+
